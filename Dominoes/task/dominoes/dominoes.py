50bded49b2e48fa0c29b0afd517a0efec0a7668c
Duc Huy LY <lyduchuy98@gmail.com>
2023-09-06 07:13:06 +0200

    stage 2 redo

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -8,13 +8,13 @@ AMOUNT_OF_STOCK_PIECES = 14
 
 def main():
     # Initialisation
-    player, computer, stock = initialise()
+    player, computer, stock = shuffle_dominos()
     snake = get_starting_domino(player, computer)
     starter = get_starting_player(computer,player)
-    print_result(stock, player, computer, snake, starter)
+    display_interface(stock, player, computer, snake, starter)
 
 
-def initialise():
+def shuffle_dominos():
     domino_set = []
     for i in range(7):
         for j in range(i, 7):
@@ -39,12 +39,34 @@ def get_starting_domino(computer_set, player_set):
     return starting_domino
 
 
-def print_result(stock, player, computer, starting_domino, starting_player):
-    print(f"Stock pieces: {stock}")
-    print(f"Computer pieces: {computer}")
-    print(f"Player pieces: {player}")
-    print(f"Domino snake: {starting_domino}")
-    print(f"Status: {starting_player}")
+def display_player_pieces(player):
+    print("Your pieces:")
+    j = 1
+    for i in player:
+        print(f"{j}:{i}")
+        j += 1
+    pass
+
+
+def display_status(starting_player):
+    if starting_player == "computer":
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+    else:
+        print("Status: It's your turn to make a move. Enter your command.")
+
+
+def display_snake(snake):
+    print(snake[0])
+    pass
+
+
+def display_interface(stock, player, computer, snake, starting_player):
+    print("="*70)
+    print(f"Stock size: {len(stock)}")
+    print(f"Computer pieces: {len(computer)}")
+    display_snake(snake)
+    display_player_pieces(player)
+    display_status(starting_player)
 
 
 def get_starting_player(computer_set, player_set):
